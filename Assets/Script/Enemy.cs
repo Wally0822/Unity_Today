77674c1d794656512ad60c787cbb630345584bb5
Wally <1506325470piao@gmail.com>
2022-11-07 14:41:46 +0900

    Audio Play

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Header("추격 속도")]
-    [SerializeField] [Range (1f, 10f)] float moveSpeed = 5f;
+    [SerializeField][Range(1f, 10f)] float moveSpeed = 5f;
+    [SerializeField][Range(10f, 50f)] float audioPlayRange = 20f;
 
     [Header("근접 거리")]
     [SerializeField][Range(1f, 10f)] float contactDistance = 1f;
 
     Transform player;
-    //[SerializeField] Transform player = null;
 
-    Rigidbody myrb = null;
+    private Rigidbody myrb = null;
+    private Animator animator = null;
+    private AudioSource enemySource = null;
+    public AudioClip workAudio = null;
 
     bool isFollow = false;
+
 
     private void Awake()
     {
         myrb = GetComponent<Rigidbody>();
+        enemySource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
-
-    void Start()
-    {
-        
     }
 
     private void Update()
@@ -36,21 +37,47 @@ public class Enemy : MonoBehaviour
     private void FollowTarget()
     {
         if (Vector3.Distance(transform.position, player.position) > contactDistance && isFollow)
+        {
             transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            transform.LookAt(player.transform);
+        }
         else
             myrb.velocity = Vector3.zero;
 
     }
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         isFollow = true;
+
+        Attack();
+
+        //if (other.tag == "Player")
+        //{
+        //}
     }
 
-    private void OnCollisionExit(Collision collision)
+
+    private void OnTriggerExit(Collider other)
     {
         isFollow = false;
+        enemySource.Stop();
+
     }
+
+
+    public void Attack()
+    {
+        //Debug.Log("잡았다 !!");
+        if (Vector3.Distance(player.transform.position, transform.position) <= audioPlayRange)
+        {
+            Debug.Log(Vector3.Distance(player.transform.position, transform.position) <= audioPlayRange);
+            enemySource.Play();
+        }
+        animator.SetBool("Attacks", true);
+
+        //other.SendMessage("Attack", SendMessageOptions.RequireReceiver);
+    }
+
 
 
 }
