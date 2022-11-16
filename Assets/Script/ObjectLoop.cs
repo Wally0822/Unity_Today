06f79ee43249d88223fedafdd588b3dbf9a5bda2
KingOneChance <111823096+KingOneChance@users.noreply.github.com>
2022-11-16 10:06:51 +0900

    ++Add Assets
    Add Assets

--- Assets/Script/ObjectLoop.cs
+++ Assets/Script/ObjectLoop.cs
@@ -6,20 +6,62 @@ public class ObjectLoop : MonoBehaviour
 {
 
     [SerializeField] float time;
-    [SerializeField] float speed;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float length = 0.5f;
     [SerializeField] float ypos;
-    [SerializeField] float length;
+
+    public int dir = 0;
+    public float up;
+    public float down;
+
     private void Awake()
     {
-        length = 0.5f;
-    }
 
+    }
+    private void Start()
+    {
+        
+    }
     void Update()
     {
+        
+        up = speed * Time.deltaTime;
+        down = -speed * Time.deltaTime;
 
-        time += Time.deltaTime * speed;
-        ypos = (Mathf.Sin(time) + 1) * length;
+        // 초당 speed의 속도로 오른쪽으로 평행이동
 
+        if (transform.localPosition.y > 4f)
+        {
+            dir = 1;
+        }
+        if (transform.localPosition.y <= 2f)
+        {
+            dir = 0;
+        }
+
+        switch (dir)
+        {
+            case 0:
+                transform.Translate(new Vector3(0, up,0));
+                break;
+            case 1:
+                transform.Translate(new Vector3(0, down,0));
+                break;
+        }
+
+        /*time += Time.deltaTime * speed;
+
+        if (ypos >= 1.7f)
+        {
+            ypos = (Mathf.Sin(time)) * -length;
+            this.transform.position = new Vector3(0, ypos, 0);
+        }
+        if (ypos <= 0.7f)
+        {
+            
+            ypos = (Mathf.Sin(time)) * length;
+            this.transform.position = new Vector3(0, ypos, 0);
+        }*/
     }
 
 
